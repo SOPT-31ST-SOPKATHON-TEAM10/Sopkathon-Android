dab0e7966e915ef2e623d5fa475d69e45547afb9
youngjinc <cyjin6789@gmail.com>
2022-11-20 05:09:22 +0900

    [feat] 온보딩 뷰 구현

--- app/src/main/java/org/sopt/sopkathon10/presentation/DummyViewModel.kt
+++ app/src/main/java/org/sopt/sopkathon10/presentation/DummyViewModel.kt
@@ -2,6 +2,7 @@ package org.sopt.sopkathon10.presentation
 
 import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.sopt.sopkathon10.data.dto.request.RequestMessage
 import org.sopt.sopkathon10.data.service.SopkathonService
 import org.sopt.sopkathon10.util.extensions.enqueueUtil
 import timber.log.Timber
@@ -12,7 +13,7 @@ class DummyViewModel @Inject constructor(
     private val sopkathonService: SopkathonService,
 ) : ViewModel() {
     fun fetchComment() {
-        sopkathonService.getComment("test").enqueueUtil({ result ->
+        sopkathonService.writeMessage(RequestMessage("", "", "", 1, "")).enqueueUtil({ result ->
             Timber.i("fetchCommentList: $result")
         }, { code ->
             Timber.d("Failed to fetch comment: $code")
