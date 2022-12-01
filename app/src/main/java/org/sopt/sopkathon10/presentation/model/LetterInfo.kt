bc370c4db7133fb894685ea78495e13b943f54d6
youngjinc <cyjin6789@gmail.com>
2022-12-01 15:32:31 +0900

    [del] 불필요한 파일 삭제

--- app/src/main/java/org/sopt/sopkathon10/presentation/model/LetterInfo.kt
+++
@@ -1,9 +0,0 @@
-package org.sopt.sopkathon10.presentation.model
-
-import org.sopt.sopkathon10.presentation.types.LetterCategory
-
-data class LetterInfo(
-    val count: Int,
-    val title: String,
-    val letterCategory: LetterCategory
-)
